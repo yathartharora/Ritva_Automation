0d96784e3ec76dfb5c06ebed19c54c06aef94377
Yatharth arora <yathartharora1999@gmail.com>
2021-08-01 15:24:53 +0530

    Automation script

--- certificate.py
+++ certificate.py
@@ -1,21 +1,20 @@
-from docx import Document
+from docxtpl import DocxTemplate
 import openpyxl
-from docx2pdf import convert
 
-loc = "./Details.xlsx"
-
-wb = openpyxl.load_workbook(loc)
+wb = openpyxl.load_workbook('./Gargi.xlsx')
 sheet = wb.active
-# print(sheet.max_row)
-# print(sheet.cell(row = 3, column =2).value)
 
 
-for i in range(4,sheet.max_row+1):
-    document = Document('./template.docx')
-    for paragraph in document.paragraphs:
-        if paragraph.text == 'Dearest $$$$$,':
-            paragraph.text = paragraph.text.replace('$$$$$',str(sheet.cell(row=i,column=2).value))
-            document.save('./Certificates/' + sheet.cell(row=i,column=2).value + '.docx')
-            break
-            
 
+def edit(x):
+    tmpl = DocxTemplate('./temp.docx')
+    context = {
+        "Name": x
+    }
+    tmpl.render(context)
+    tmpl.save('./EarthlyArtsy/' + x + '.docx')
+
+
+for i in range(45,56):
+    cell = sheet.cell(row=i,column=1)
+    edit(cell.value)
